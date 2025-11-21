2ef3a54cf545becce7d36eb0b0dd5d80ebd53c92
Sahan Sachintha <ks.official.sahan@gmail.com>
2025-11-21 15:12:12 +0530

    feat(cache): implement caching strategy for event details and similar events; update configuration for cache components

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -15,7 +15,7 @@ export const getSimilarEventsBySlug = async (slug: string) => {
     return await Event.find({
       _id: { $ne: event._id },
       tags: { $in: event.tags },
-    }).lean();
+    }).lean(); // need to convert to plain JS objects for serialization // without this, Next.js will throw an error about empty strings in the response. // This needs to be done in server actions (required) and in API routes (optional in json response).
   } catch (error) {
     console.error("Error fetching similar events:", error);
     return [];
